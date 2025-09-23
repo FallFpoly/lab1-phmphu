18587abac40b262c8568be8e533803df1056b8a4
Admin <Admin@172.16.106.35>
2025-09-23 14:57:04 +0700

    nộp lab1

--- Lab1/src/main/java/com/mycompany/lab1/bai4.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai4.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -12,5 +14,15 @@ public class bai4 {
 
     public static void main(String[] args) {
           // sinh viên code ở đây
+          Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("nhap a: ");
+        a=sc.nextInt();
+        System.out.println("nhap b: ");
+        b=sc.nextInt();
+        System.out.println("nhap c: ");
+        c=sc.nextInt();
+        double TrungBinh=(a+b+c)/3;
+        System.out.println("Trung binh: "+TrungBinh);
     }
 }
